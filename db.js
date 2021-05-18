d25feadeb4b11ee457a0e76988d9e9df8631d703
Ilya Kozak <kozak@tut.by>
2021-05-18 15:50:36 +0300

    fix: add port number to Sequelize init for DB connection

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const { DB_USER, DB_PASS } = process.env;
 const sequelize = new Sequelize('gamedb', DB_USER, DB_PASS, {
     host: 'localhost',
+    port: 5433,
     dialect: 'postgres'
 })
 
